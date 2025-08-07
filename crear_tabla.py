5292e6033557ebfca18dacb12cb244794a061029
DavidInsano777 <araizadavid712@gmail.com>
2025-08-06 22:38:15 -0600

    💻 Agregada vista HTML para mostrar lecturas con estilo

--- crear_tabla.py
+++ crear_tabla.py
@@ -14,10 +14,10 @@ try:
     cursor = conn.cursor()
 
     cursor.execute("""
-        CREATE TABLE lecturas3 (
+        CREATE TABLE lecturas2 (
             id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             humedad INT NOT NULL,
-            riego TINYINT(1) NOT NULL,
+            riego VARCHAR(20),
             fecha DATETIME
         );
     """)
